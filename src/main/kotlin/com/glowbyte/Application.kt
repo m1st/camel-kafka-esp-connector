43f2ac7c7655dc0398b8f05d3b7a8b30cc12f018
Alexey Lebedev <lex.altair@gmail.com>
2018-08-27 11:43:53 +0300

    1. ESP to MC route

---
+++ src/main/kotlin/com/glowbyte/Application.kt
@@ -0,0 +1,27 @@
+package com.glowbyte
+
+import com.ibm.mq.jms.MQConnectionFactory
+import org.apache.camel.component.jms.JmsComponent
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+
+@SpringBootApplication
+class Application {
+    @Autowired
+    lateinit var cf: MQConnectionFactory
+
+    @Bean(name = ["jms"])
+    fun jmsComponent(): JmsComponent {
+        val jmsc = JmsComponent()
+        jmsc.setConnectionFactory(cf)
+        return jmsc
+    }
+}
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
